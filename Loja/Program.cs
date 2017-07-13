7d5eed47771f0da3f9b7ed19aeff8a3e62ab903d
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-07-13 01:12:51 -0300

    Add project files.

---
+++ Loja/Program.cs
@@ -0,0 +1,19 @@
+﻿using Loja.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NHibernateHelper.GeraSchema();
+
+            Console.Read();
+        }
+    }
+}
